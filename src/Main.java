cc9e3dc547bd4abc72f6b543180676aeb0f43b77
377Nika2004 <borcovaveronika337@gmail.com>
2024-10-21 00:58:42 +0300

    Решение задач по циклам 1

--- src/Main.java
+++ src/Main.java
@@ -79,6 +79,7 @@ public class Main {
         }
         //Напишите программу, которая выводит в консоль таблицу умножения на 2
         System.out.println("Задача 10");
+
         for (int i = 1; i <= 10; i++) {
             int result = 2 * i;
             System.out.println("2 * " + i + " = " + result);
